f8b8494aed095771d56b8dbba05f7842943eed8e
Johannes Laire <johannes@laire.fi>
2013-05-28 05:55:19 +0300

    clean up

--- sudoku/cli.js
+++ sudoku/cli.js
@@ -2,40 +2,41 @@
 var cli = require("cli");
 var solver = require("./solver.js");
 
-function usage() {
-	this.output("No sudokus given\n");
-}
-
 function solveSudoku(sudoku) {
-	var array = sudoku.split("").map(Number);
-
+	var solutions;
 	try {
-		var solutions = solver.solve(array);
-		if (solutions.length === 0) {
-			this.output("No solutions\n");
-		}
-		else if (solutions.length === 1) {
-			this.output(solutions[0].join("") + "\n");
-		}
-		else {
-			this.output(solutions.length + " solutions, here's one:");
-			this.output(solutions[0].join("") + "\n");
-		}
+		solutions = solver.solve(sudoku);
 	}
 	catch (ex) {
 		this.output("Invalid sudoku: " + ex + "\n");
+		return;
+	}
+
+	if (solutions.length === 0) {
+		this.output("No solutions\n");
+	}
+	else if (solutions.length === 1) {
+		this.output(solutions[0].join("") + "\n");
+	}
+	else {
+		this.output(solutions.length + " solutions, here's one:");
+		this.output(solutions[0].join("") + "\n");
 	}
 }
 
 cli.withStdin(function (input) {
 	var digits = input.replace(/[^\d]+/g, "");
-	var sudokus = digits.match(/(\d{81})/g);
-	if (!sudokus || sudokus.length === 0) {
-		usage.call(this);
+	var strings = digits.match(/(\d{81})/g);
+	if (!strings || strings.length === 0) {
+		this.output("No sudokus given\n");
 		return;
 	}
 	if (digits.length % 81 !== 0) {
-		this.output("warn: garbage input\n");
+		this.output("warn: trailing digits in input\n");
 	}
+
+	var sudokus = strings.map(function (string) {
+		return string.split("").map(Number);
+	});
 	sudokus.forEach(solveSudoku.bind(this));
 });
--- sudoku/solver.js
+++ sudoku/solver.js
@@ -1,47 +1,23 @@
 var dlx = require("dlx");
 var _ = require("underscore");
 
-function row_column_constraint(i) {
-	var x = i % 9;
-	var y = Math.floor(i / 9);
-	return x + "," + y;
-}
-
-function row_number_constraint(i, n) {
-	var y = Math.floor(i / 9);
-	return n + "@R" + y;
-}
-
-function column_number_constraint(i, n) {
-	var x = i % 9;
-	return n + "@C" + x;
-}
-
-function box_number_constraint(i, n) {
+function i_n_to_constraints(i, n) {
 	var x = i % 9;
 	var y = Math.floor(i / 9);
 	var box = Math.floor(y / 3) * 3 + Math.floor(x / 3);
-	return n + "@B" + box;
-}
-
-function i_n_to_constraints(i, n) {
 	return [
-		row_column_constraint(i),
-		row_number_constraint(i, n),
-		column_number_constraint(i, n),
-		box_number_constraint(i, n)
+		x + "," + y,
+		n + "@R" + y,
+		n + "@C" + x,
+		n + "@B" + box
 	];
-}
-
-function row_id(i, n) {
-	return n + "@" + i;
 }
 
 function constraints(sudoku) {
 	var constraints = {};
 	for (var i = 0; i < 81; ++i) {
 		var n = sudoku[i];
-		if (typeof n === 'number' && n >= 1 && n <= 9) {
+		if (typeof n === "number" && n >= 1 && n <= 9) {
 			var cs = i_n_to_constraints(i, n);
 			_.each(cs, function (c) {
 				if (constraints[c]) {
@@ -67,16 +43,13 @@ function solve(sudoku) {
 					possible = false;
 				}
 			});
-			if (!possible) {
-				continue;
+			if (possible) {
+				rows[n + "@" + i] = cs;
 			}
-
-			rows[row_id(i, n)] = cs;
 		}
 	}
 
-	var solutions = dlx.solve_tagged_matrix(rows);
-	return solutions.map(show_solution.bind(null, sudoku));
+	return dlx.solve_tagged_matrix(rows).map(show_solution.bind(null, sudoku));
 }
 
 function show_solution(sudoku, solution) {
