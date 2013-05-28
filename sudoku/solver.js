7f52dae8d2cff1f0e4affda8ef685b48ac09cd2e
Johannes Laire <johannes@laire.fi>
2013-05-28 05:39:30 +0300

    use new dlx.solve_tagged_matrix()

--- sudoku/solver.js
+++ sudoku/solver.js
@@ -37,10 +37,6 @@ function row_id(i, n) {
 	return n + "@" + i;
 }
 
-function constraint_to_index(c) {
-	return ALL_CONSTRAINTS[c];
-}
-
 function constraints(sudoku) {
 	var constraints = {};
 	for (var i = 0; i < 81; ++i) {
@@ -56,19 +52,6 @@ function constraints(sudoku) {
 		}
 	}
 	return constraints;
-}
-
-function convert_columns_to_numbers(rows) {
-	var cols = {};
-	_.flatten(_.values(rows)).forEach(function (col) {
-		cols[col] = true;
-	});
-	cols = _.keys(cols).sort();
-	_.each(_.keys(rows), function (key) {
-		rows[key] = _.map(rows[key], function (col) {
-			return cols.indexOf(col);
-		});
-	});
 }
 
 // `sudoku` is an array of length 81 containing numbers 0-9
@@ -92,23 +75,14 @@ function solve(sudoku) {
 		}
 	}
 
-	convert_columns_to_numbers(rows);
-	var row_ids = Object.keys(rows);
-	var sparse_matrix = row_ids.map(function (id) {
-		return rows[id];
-	});
-
-	console.log("Calling dlx.solve...");
-	var solutions = dlx.solve_sparse_matrix(sparse_matrix);
-	return solutions.map(function (solution) {
-		return show_solution(solution, row_ids, sudoku);
-	});
+	var solutions = dlx.solve_tagged_matrix(rows);
+	return solutions.map(show_solution.bind(null, sudoku));
 }
 
-function show_solution(chosen_rows, row_ids, sudoku) {
+function show_solution(sudoku, solution) {
 	sudoku = [].concat(sudoku);
-	chosen_rows.forEach(function (row) {
-		var match = row_ids[row].match(/(\d+)@(\d+)/);
+	solution.forEach(function (row) {
+		var match = row.match(/(\d+)@(\d+)/);
 		var n = parseInt(match[1], 10);
 		var i = parseInt(match[2], 10);
 		sudoku[i] = n;
