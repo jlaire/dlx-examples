eb0348ce994f105672a05e235385faf2dcb9398e
Johannes Laire <johannes@laire.fi>
2013-05-28 06:30:39 +0300

    more readable output

--- sudoku/cli.js
+++ sudoku/cli.js
@@ -2,6 +2,10 @@
 var cli = require("cli");
 var solver = require("./solver.js");
 
+function showSolution(sudoku) {
+	return sudoku.join("").match(/(\d{9})/g).join("\n");
+}
+
 function solveSudoku(sudoku) {
 	var solutions;
 	try {
@@ -14,14 +18,14 @@ function solveSudoku(sudoku) {
 
 	if (solutions.length === 0) {
 		this.output("No solutions\n");
+		return;
 	}
-	else if (solutions.length === 1) {
-		this.output(solutions[0].join("") + "\n");
+
+	if (solutions.length > 1) {
+		this.output("Found " + solutions.length + " solutions, here's one:\n");
 	}
-	else {
-		this.output(solutions.length + " solutions, here's one:");
-		this.output(solutions[0].join("") + "\n");
-	}
+
+	this.output(showSolution(solutions[0]) + "\n");
 }
 
 cli.withStdin(function (input) {
